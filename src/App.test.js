fcd419c5d1e2811b52a59d1a2177b37a2916d148
Yoon321 <tkdwls734@gmail.com>
2024-06-13 15:32:52 +0900

    [fix] ci 에러 테스트

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom'; // BrowserRouter 추가
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <Router> {/* App을 BrowserRouter로 감쌉니다 */}
+      <App />
+    </Router>
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
