22cea8434c6df57b5ce117f77b8fd187f7f10703
Yoon321 <tkdwls734@gmail.com>
2024-06-13 15:37:56 +0900

    [fix] 에러 테스트

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom'; // BrowserRouter 추가
 import App from './App';
+import { act } from 'react'; // React에서 act 가져오기
 
-test('renders learn react link', () => {
-  render(
-    <Router> {/* App을 BrowserRouter로 감쌉니다 */}
-      <App />
-    </Router>
-  );
-  const linkElement = screen.getByText(/learn react/i);
+test('renders specific text', async () => {
+  await act(async () => {
+    render(
+      <Router>
+        <App />
+      </Router>
+    );
+  });
+  
+  // 실제로 존재하는 텍스트로 테스트를 수정합니다.
+  const linkElement = screen.getByText(/하랑 인테리어/i);
   expect(linkElement).toBeInTheDocument();
 });
